a5857b631122a241d764c5bb1b0a3a73648524a6
Phillip Raffnsoe <phillip@praffn.dk>
2019-02-14 09:58:24 +0100

    Updates

---
+++ precache-manifest.9514bf32ab19099b49748e34f8a5a270.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a479d153b4f3bb700631",
+    "url": "/itu-schedule/static/js/runtime~main.a479d153.js"
+  },
+  {
+    "revision": "cb505e0018655c362d47",
+    "url": "/itu-schedule/static/js/main.cb505e00.chunk.js"
+  },
+  {
+    "revision": "efc9df46b07f41899c35",
+    "url": "/itu-schedule/static/js/2.efc9df46.chunk.js"
+  },
+  {
+    "revision": "02f466824b88f5b62bfa9b61e8cd2f87",
+    "url": "/itu-schedule/index.html"
+  }
+];
